192d80ace9a0121b2479b0cf9b304da5908874e5
Caio Casagrande <87882726+caiocasagrande@users.noreply.github.com>
2024-05-12 20:53:05 -0300

    Uploading App file | Streamlit #02

--- App.py
+++ App.py
@@ -26,34 +26,42 @@ gdf = gpd.GeoDataFrame.from_file('data/processed/crcrs_delegados.geojson')
 
 # ---------- Main ----------
 
-# Transforming dataset
+# Selecting columns
 gdf = gdf[['nm_mun', 'jurisdicao', 'nm_delegado', 'geometry']]
 
-gdf = gdf.set_index('nm_mun')
+# Add number of reference
+gdf['id_jurisdicao'] = pd.factorize(gdf['jurisdicao'])[0] + 1
+
+# Set index
+gdf.set_index('nm_mun', inplace=True)
 
 # ---------- Streamlit App Code ----------
 
 # Set the background color of the Streamlit app
-st.set_page_config(layout="wide", page_title="CRCRS", page_icon="💻", 
-                   initial_sidebar_state="collapsed")
+st.set_page_config(layout="wide", page_title="Delegados | CRC-RS", page_icon="📈", 
+                   initial_sidebar_state="expanded")
 
-st.sidebar.markdown('# CRCRS')
+st.sidebar.markdown('# CRC-RS')
 st.sidebar.markdown("""---""")
 
-st.header('CRCRS')
+st.header('CRC-RS')
 
-st.subheader('Delegados CRCRS')
+st.subheader('Delegados CRC-RS')
 
 fig = px.choropleth_mapbox(gdf, 
                         geojson=gdf.geometry, 
                         locations=gdf.index,
-                        color='jurisdicao', 
-                        hover_name='nm_delegado', 
+                        color='nr_delegacia', 
+                        color_continuous_scale='Portland',
+                        hover_name='delegacia', 
                         hover_data=['nm_delegado'],
                         mapbox_style='carto-positron', 
-                        center = {'lat': -30.033333, 'lon': -51.15},
-                        zoom=5.5, 
-                        title='CRCRS Delegados - RS'
+                        center = {'lat': -31, 'lon': -53.5},
+                        zoom=5,
+                        labels={'nm_mun': 'Município', 
+                                'jurisdicao': 'Delegacia', 
+                                'nm_delegado': 'Delegado', 
+                                'id_jurisdicao': 'ID'}
 )
 
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
