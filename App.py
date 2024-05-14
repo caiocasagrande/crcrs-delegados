56dcc0c332ccad47c3e541c1c590cb1af7f94946
Caio Casagrande <87882726+caiocasagrande@users.noreply.github.com>
2024-05-14 11:35:45 -0300

    Updating App file | Streamlit #05

--- App.py
+++ App.py
@@ -1,18 +1,19 @@
 # ---------- Importing libraries ----------
  
 # Data manipulation libraries
-import pandas           as pd
-import numpy            as np
+import pandas                   as pd
+import numpy                    as np
 
 # Data visualization libraries
-import plotly.express   as px
-import matplotlib       as mpl
+import plotly.express           as px
+import matplotlib               as mpl
+import plotly.graph_objects     as go
 
 # Geolocation libraries
-import geopandas        as gpd
+import geopandas                as gpd
 
 # Other
-import streamlit        as st
+import streamlit                as st
 import warnings
 
 # ---------- Settings ----------
@@ -42,8 +43,10 @@ df = pd.DataFrame(gdf[['nm_mun', 'jurisdicao', 'nm_delegado']])
 gdf = gdf[['nm_mun', 'jurisdicao', 'nm_delegado', 'geometry']]
 # Add number of reference
 gdf['id_jurisdicao'] = pd.factorize(gdf['jurisdicao'])[0] + 1
+# Copy of 'gdf'
+gdf1 = gdf.copy()
 # Set index
-gdf.set_index('nm_mun', inplace=True)
+gdf1 = gdf1.set_index('nm_mun', inplace=True)
 
 # ---------- Streamlit App Code ----------
 
@@ -64,14 +67,15 @@ with sidebar_container:
         """
         Este aplicativo permite que você pesquise por **delegados** e **delegacias** do **Conselho Regional de Contabilidade do Rio Grande do Sul** e seus municípios de atividade.
 
-        O aplicativo é composto por duas seções: **Pesquisa** e **Mapa RS**. 
+        O aplicativo é composto por três seções: **Pesquisa**, **Mapa RS** e **Pesquisa no Mapa**. 
         
         Selecionando a seção **Pesquisa**, você pode pesquisar por município, delegacia ou delegado e isso retornará as informações correspondentes.  
         
         Ao selecionar a seção **Mapa RS**, será exibido o mapa do estado do Rio Grande do Sul colorido conforme as 103 delegacias registradas. 
         
         No mapa, ao passar o cursor do *mouse* sobre cada município, serão exibidas as informações de cada um.
-        
+
+        Na última seção, Pesquisa no Mapa, é possível selecionar a opção de pesquisa de seu interesse e o município correspondente estará com seu limite destacado em vermelho no mapa.
         """
     )
     st.markdown("---")
@@ -81,7 +85,7 @@ with sidebar_container:
 # Main
 st.header('Delegados e Delegacias do CRCRS')
 
-tab1, tab2 = st.tabs(['Pesquisa', 'Mapa RS'])
+tab1, tab2, tab3 = st.tabs(['Pesquisa', 'Mapa RS', 'Pesquisa no Mapa'])
 
 with tab1:
         # ---------- Pesquisa por município ----------
@@ -107,7 +111,7 @@ with tab1:
 
         # ---------- Pesquisa por Delegacia ----------
         st.markdown('### Pesquisa por delegacia')
-        selected_delegacy = st.selectbox('**Selecionar:**', df['jurisdicao'].unique())
+        selected_delegacy = st.selectbox('**Selecionar:**', df['jurisdicao'].sort_values().unique())
 
         # Filter the DataFrame 
         filtered_by_delegacy = df[df['jurisdicao'] == selected_delegacy]
@@ -130,7 +134,7 @@ with tab1:
 
         # ---------- Pesquisa por Delegado ----------
         st.markdown('### Pesquisa por delegado')
-        selected_responsible = st.selectbox('**Selecionar:**', df['nm_delegado'].unique())
+        selected_responsible = st.selectbox('**Selecionar:**', df['nm_delegado'].sort_values().unique())
 
         # Filter the DataFrame 
         filtered_by_delegate = df[df['nm_delegado'] == selected_responsible]
@@ -156,13 +160,13 @@ with tab2:
         st.markdown('### Mapa de delegados por delegacia e município')
 
         # Create the map
-        fig = px.choropleth_mapbox(gdf, 
-                                geojson=gdf.geometry, 
-                                locations=gdf.index,
+        fig = px.choropleth_mapbox(gdf1, 
+                                geojson=gdf1.geometry, 
+                                locations=gdf1.index,
                                 color='id_jurisdicao', 
                                 color_continuous_scale='Portland',
                                 hover_name='jurisdicao', 
-                                hover_data=['jurisdicao', 'nm_delegado'],
+                                hover_data=['nm_delegado'],
                                 mapbox_style='carto-positron', 
                                 center = {'lat': -31, 'lon': -53.5},
                                 zoom=5,
@@ -177,3 +181,78 @@ with tab2:
 
         # Display the map
         st.plotly_chart(fig, use_container_width=True)
+ 
+        st.markdown("---") 
+        
+        # Footer
+        st.markdown(
+            """
+            - Cada cor representa uma delegacia
+            - A informação principal é o nome da delegacia
+            - As informações secundárias são o nome do município e o nome do delegado
+            """
+        )
+
+with tab3: 
+        # Name
+        st.markdown('### Pesquisa no Mapa')
+
+        # Streamlit select box for filtering
+        option_type = st.selectbox('Selecione o tipo para filtrar:', ['Município', 'Delegacia', 'Delegado'])
+
+        option_type_dict = {
+                'Município': 'nm_mun', 
+                'Delegacia': 'jurisdicao', 
+                'Delegado':  'nm_delegado'
+        }
+
+        column_name = option_type_dict[option_type]
+        selected_option = st.selectbox('Selecione a opção:', gdf[column_name].sort_values().unique())
+
+        # Filter the GeoDataFrame based on selected option
+        filtered_gdf = gdf[gdf[column_name] == selected_option]
+
+        # Create the map
+        fig = px.choropleth_mapbox(gdf, 
+                                geojson=gdf.geometry, 
+                                locations=gdf.index,
+                                color='id_jurisdicao', 
+                                color_continuous_scale='Portland',
+                                hover_name='jurisdicao', 
+                                hover_data=['nm_delegado'],
+                                mapbox_style='carto-positron', 
+                                center = {'lat': -31, 'lon': -53.5},
+                                zoom=5,
+                                labels={'nm_mun': 'Município', 
+                                        'jurisdicao': 'Delegacia', 
+                                        'nm_delegado': 'Delegado', 
+                                        'id_jurisdicao': 'ID'}
+        )
+
+        # Add highlight for the selected polygon
+        if not filtered_gdf.empty:
+                highlight_polygon = filtered_gdf.iloc[0].geometry
+
+        # Convert polygon to scatter plot data
+                lons, lats = highlight_polygon.exterior.coords.xy
+                lons = list(lons)
+                lats = list(lats)
+
+                fig.add_trace(go.Scattermapbox(
+                        lat=lats,
+                        lon=lons,
+                        mode='lines',
+                        line=dict(width=2, color='red'),
+                        name=selected_option
+                ))
+
+        # Map layout
+        fig.update_layout(
+                margin={"r":0,"t":0,"l":0,"b":0},
+                mapbox=dict(center={'lat': -31, 'lon': -53.5}, zoom=5)
+        )
+
+        # Display the map
+        st.plotly_chart(fig, use_container_width=True)
+
+        st.markdown("---")
